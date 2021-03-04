fca9b6fc4be3461193627d6989103d399e4cb216
Максим Тимохин <maximtim@outlook.com>
2021-03-04 16:26:50 +0300

    fixed

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,5 @@
 int main() {
     Runner runner(stdin);
     runner.Go();
-    // Tokenizer tok(stdin);
-    // do {
-    //     tok.Next();
-    //     if (tok.IsEnd()) {
-    //         break;
-    //     }
-    //     auto token = tok.GetToken();
-    //     if (std::holds_alternative<SpaceToken>(token)) {
-    //         std::cout << "<Space> ";
-    //     }
-    //     if (std::holds_alternative<NewLineToken>(token)) {
-    //         std::cout << "<NewLine>\n";
-    //     }
-    //     if (std::holds_alternative<PipeToken>(token)) {
-    //         std::cout << "<Pipe> ";
-    //     }
-    //     if (auto constant = std::get_if<ConstantToken>(&token); constant) {
-    //         std::cout << "<Constant " << constant->value << "> ";
-    //     }
-    //     if (auto constant = std::get_if<ExpressionToken>(&token); constant) {
-    //         std::cout << "<Expression " << constant->expression << "> ";
-    //     }
-    // } while (true);
     return 0;
 }
